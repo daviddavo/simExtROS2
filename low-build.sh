50252ca8cf9d7b76311aad1f68d220a888e90bee
David Davó <david@ddavo.me>
2022-12-05 11:18:56 +0100

    New low-build.sh

--- low-build.sh
+++ low-build.sh
@@ -4,6 +4,7 @@ export MAKEFLAGS="-j1 -l1"
 
 # cpulimit --limit 10 --monitor-forks --foreground -- \
 
-systemd-run --user --scope -p CPUQuota="5%" \
-    colcon build --symlink-install --merge-install
+systemd-run --uid=$(id -u) --gid=$(id -g) --scope \
+    -p AllowedCPUs=0 -p CPUWeight=10 -p CPUQuota="100%" \
+    colcon build --parallel-workers 1 --symlink-install --merge-install
 
